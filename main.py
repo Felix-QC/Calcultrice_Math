0e8dd1b681eae9117d5264693bcca720ab2d1223
Félix <51272629+Felix-QC@users.noreply.github.com>
2020-05-30 16:48:53 -0400

    add a new option

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ def main():
 ##  3. Calculer l'aire d'un disque                   ##
 ##  4. Calculer le volume d'un cube                  ##
 ##  5. Calculer le volume d'un solide triangulaire   ##
+##  6. Calculer le volume d'un cylindre              ##
+##  7. Calculer le volume d'un prisme                ##
 #######################################################
-##  6. Quitter le programme                          ##
+##  8. Quitter le programme                          ##
 #######################################################""")
             user_input = int(input("\nEntrer le numero associer a l'option : \n"))
         except ValueError:
@@ -31,13 +33,17 @@ def main():
             print("\nErreur : Vous devez entrer un nombre entier.\n")
             time.sleep(2.45)
             continue
-        if user_input == 6:
+        if user_input == 8:
             clr()
             print("Merci de m'avoir utiliser !")
             break
-        if user_input >= 7:
+        if user_input >= 9:
             print("\nErreur : Option non valide\n")
             time.sleep(2.45)
+            continue
+        if user_input == 7: # Will be there later
+            print("\nOption pas encore disponible\n")
+            time.sleep(5.5) 
             continue
         ordre_grandeur_input = str(input("\nEntrer l'unite de mesure ex:(cm, mm, m) : \n"))
         try:
@@ -99,12 +105,6 @@ def main():
             print("\nLa reponse est :", ((arret_input * arret_input) * 6), f"{ordre_grandeur_input.lower()}²")
             time.sleep(5.5) 
             continue
-
-
-
-
-
-
         if user_input == 5:
             cls()
             print("""
@@ -159,6 +159,25 @@ def main():
                 print("\nLa reponse est :", ((resultat * hauteur_2_input) / 3), f"{ordre_grandeur_input.lower()}³")
                 time.sleep(5.5) 
                 continue
+        if user_input == 6:
+            try:                       
+                rayon_input = int(input("\nQuels est la grandeur du Rayon d'une base ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
+                continue             
+            resultat = (rayon_input * rayon_input)
+            try:
+                hauteur_input = int(input("\nQuels est sa Hauteur ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
+                continue
+            resultat_hauteur = resultat * hauteur_input
+            resultat_last = resultat_hauteur + ((resultat)* 2)
+            print("\nLa reponse est :", (resultat_last * math.pi), f"{ordre_grandeur_input.lower()}³")
+            time.sleep(5.5) 
+            continue
 
 if __name__ == "__main__":
     main()
