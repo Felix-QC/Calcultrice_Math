f8ae7b023032406dd66c6e822487925ded28d50b
Félix <51272629+Felix-QC@users.noreply.github.com>
2020-05-30 15:31:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #coding utf-8
 #librarys
-import os
 import math
 import time
 # Function
@@ -29,64 +28,83 @@ def main():
             user_input = int(input("\nEntrer le numero associer a l'option : \n"))
         except ValueError:
             clr()
-            print("Erreur : Vous devez entrer un nombre entier.")
+            print("\nErreur : Vous devez entrer un nombre entier.\n")
             time.sleep(2.45)
             continue
         if user_input == 6:
             clr()
             print("Merci de m'avoir utiliser !")
             break
+        if user_input >= 7:
+            print("\nErreur : Option non valide\n")
+            time.sleep(2.45)
+            continue
         ordre_grandeur_input = str(input("\nEntrer l'unite de mesure ex:(cm, mm, m) : \n"))
         try:
             int(ordre_grandeur_input)
-            print("Erreur : Vous devez entrer une unitee de mesure valide.")
+            print("\nErreur : Vous devez entrer une unitee de mesure valide.\n")
             time.sleep(2.65)
             continue  
         except:
-            pass           
+            pass
         if user_input == 1:
-            base_input = int(input("\nQuels est la grandeur de sa Base ? : \n"))
-            hauteur_input = int(input("\nQuels est sa Hauteur ? : \n"))
-            print("\nLa reponse est :", (base_input * hauteur_input) / 2, ordre_grandeur_input)
-            stop_program = str(input("\nVoulez vous continuez ? (y, n) : \n"))
-            if stop_program.startswith("y"):
+            try:
+                base_input = int(input("\nQuels est la grandeur de sa Base ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45)
                 continue
-            else:
-                cls()                
-                print("Merci de m'avoir utiliser !")
-                break
+            try:
+                hauteur_input = int(input("\nQuels est sa Hauteur ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
+                continue
+            print("\nLa reponse est :", (base_input * hauteur_input) / 2, ordre_grandeur_input.lower())
+            time.sleep(5.5) 
+            continue
         if user_input == 2:
-            base_input = int(input("\nQuels est la grandeur de sa Base ? : \n"))
-            hauteur_input = int(input("\nQuels est sa Hauteur ? : \n"))
-            print("\nLa reponse est :", (base_input * hauteur_input), ordre_grandeur_input)
-            stop_program = str(input("\nVoulez vous continuez ? (y, n) : \n"))
-            if stop_program.startswith("y"):
+            try:   
+                base_input = int(input("\nQuels est la grandeur de sa Base ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
                 continue
-            else:
-                cls()                
-                print("Merci de m'avoir utiliser !")
-                break
+            try:   
+                hauteur_input = int(input("\nQuels est sa Hauteur ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
+                continue
+            print("\nLa reponse est :", (base_input * hauteur_input), ordre_grandeur_input.lower())
+            time.sleep(5.5) 
+            continue
         if user_input == 3:
-            rayon_input = int(input("\nQuels est la grandeur du Rayon ? : \n"))
-            float(rayon_input)
-            print("\nLa reponse est :", (math.pi * (rayon_input * rayon_input)), ordre_grandeur_input)
-            stop_program = str(input("\nVoulez vous continuez ? (y, n) : \n"))
-            if stop_program.startswith("y"):
+            try:
+                rayon_input = int(input("\nQuels est la grandeur du Rayon ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
                 continue
-            else:
-                cls()
-                print("Merci de m'avoir utiliser !")
-                break
+            print("\nLa reponse est :", (math.pi * (rayon_input * rayon_input)), ordre_grandeur_input.lower())
+            time.sleep(5.5) 
+            continue
         if user_input == 4:
-            arret_input = int(input("\nQuels est la grandeur d'une arret ? : \n"))
-            print("\nLa reponse est :", ((arret_input * arret_input) * 6), f"{ordre_grandeur_input}²")
-            stop_program = str(input("\nVoulez vous continuez ? (y, n) : \n"))
-            if stop_program.startswith("y"):
+            try:
+                arret_input = int(input("\nQuels est la grandeur d'une arret ? : \n"))
+            except ValueError:
+                print("\n\nErreur : Vous devez entrer un nombre entier.\n")
+                time.sleep(2.45) 
                 continue
-            else:
-                cls()
-                print("Merci de m'avoir utiliser !")
-                break
+            print("\nLa reponse est :", ((arret_input * arret_input) * 6), f"{ordre_grandeur_input.lower()}²")
+            time.sleep(5.5) 
+            continue
+
+
+
+
+
+
         if user_input == 5:
             cls()
             print("""
@@ -101,20 +119,14 @@ def main():
                 hauteur_input = int(input("\nQuels est la Hauteur (1 face seulement) ? : \n"))
                 resultat = (base_input * hauteur_input) / 2
                 hauteur_2_input = int(input("Quels est la Hauteur de la pyramide ? : \n"))
-                print("\nLa reponse est :", ((resultat * hauteur_2_input) / 3), f"{ordre_grandeur_input}³")
+                print("\nLa reponse est :", ((resultat * hauteur_2_input) / 3), f"{ordre_grandeur_input.lower()}³")
             else:
                 rayon_input = int(input("\nQuels est la grandeur du Rayon de la base ? : \n"))
                 float(rayon_input)
                 resultat = math.pi * (rayon_input * rayon_input)
                 hauteur_2_input = int(input("\nQuels est la Hauteur du cone ? : \n"))
-                print("\nLa reponse est :", ((resultat * hauteur_2_input) / 3), f"{ordre_grandeur_input}³")
-            stop_program = str(input("\nVoulez vous continuez ? (y, n) : \n"))
-            if stop_program.startswith("y"):
-                continue
-            else:
-                cls()
-                print("Merci de m'avoir utiliser !")
-                break
+                print("\nLa reponse est :", ((resultat * hauteur_2_input) / 3), f"{ordre_grandeur_input.lower()}³")
+
 
 if __name__ == "__main__":
     main()
